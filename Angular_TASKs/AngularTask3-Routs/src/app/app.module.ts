ccceaae7e6b7743fdea8515bbf13238c50fc1b93
TarikoDan <62624974+TarikoDan@users.noreply.github.com>
2020-07-08 02:30:21 +0300

    router-outlet

--- Angular_TASKs/AngularTask3-Routs/src/app/app.module.ts
+++ Angular_TASKs/AngularTask3-Routs/src/app/app.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
 import { UserComponent } from './components/user/user.component';
-import {UserService} from './services/user.service';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes =  [
+  {path: 'users', component: UserComponent}
+];
 
 @NgModule({
   declarations: [
@@ -13,9 +17,10 @@ import {HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [UserService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
